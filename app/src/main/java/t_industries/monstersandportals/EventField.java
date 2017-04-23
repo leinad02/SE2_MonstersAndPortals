3080a83bea24e044067356c41cc5d25b56c4644f
Amsi11 <jasmin.stuck@live.de>
2017-04-23 11:38:31 +0200

    EventField umgeschrieben, was in LIBGDX war.

---
+++ app/src/main/java/t_industries/monstersandportals/EventField.java
@@ -0,0 +1,52 @@
+package t_industries.monstersandportals;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import static java.lang.String.valueOf;
+
+/**
+ * Created by Jasmin on 22.04.2017.
+ */
+
+public class EventField extends AppCompatActivity {
+    Random random = new Random();
+    private int number = (random.nextInt(10) + 1);
+    //Randomzahl zwischen 1 und 10
+    private String num = valueOf(number);
+    private int move = 4; //wenn richtig, den Player 4 Positionen vor schicken
+
+    //Nummer muss angezeigt werden in dem numberfeld
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+        TextView output=(TextView)findViewById(R.id.output);
+        output.setText(num);
+    }
+
+    public void checkstraight(View v){
+        do {
+            if (number == 0) {
+
+                //Spieler bewegen und zurück
+            } else {
+                //keine Reaktion und zurück aufs Gamefeld
+            }
+        }while(number>1);
+    }
+
+    public void checkodd(View v){
+        do {
+            if (number == 1) {
+                //Spieler bewegen und zurück
+            } else {
+                //keine Reaktion
+            }
+        }while(number>1);
+    }
+}
--- app/src/main/java/t_industries/monstersandportals/MenuActivity.java
+++ app/src/main/java/t_industries/monstersandportals/MenuActivity.java
@@ -10,4 +10,5 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
 }
