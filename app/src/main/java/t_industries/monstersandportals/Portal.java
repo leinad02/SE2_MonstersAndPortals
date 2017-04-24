a20d03a2ac11930666b5ed1ef34338d72a942c0a
SaW2017 <littlemiss0085@yahoo.de>
2017-04-24 14:07:16 +0200

    Animation des Portals funktioniert nun, aber der Timer leider nicht. Weiterhin habe ich irgendetwas übersehen bei der Funktion des Buttons. Ich komme nicht zurück in die Main Activity.
    --> dasselbe Problem besteht weiterhin, habe bisweilen das WebView gelöscht.

--- app/src/main/java/t_industries/monstersandportals/Portal.java
+++ app/src/main/java/t_industries/monstersandportals/Portal.java
@@ -11,7 +11,8 @@ import android.widget.Button;
  * Created by SW on 23.04.2017.
  */
 
-public class Portal extends AppCompatActivity implements View.OnClickListener {
+
+public class Portal extends AppCompatActivity {
 
     int position;
     Button portal;
@@ -22,18 +23,36 @@ public class Portal extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.portal);
+
         portal = (Button)findViewById(R.id.portalBtn);
-        portal.setOnClickListener(this);
+        portal.setOnClickListener(new View.OnClickListener() {
 
-       /* webView = (WebView) findViewById(R.id.webView);
-        webView.loadUrl("file:///C:/Users/SW/AndroidStudioProjects/SE2_MonstersAndPortals_NEUohneLibGDX/app/src/main/assets/portal2.html");
-*/
+            @Override
+            public void onClick(View v) {
 
-        Thread timer = new Thread(){
+        /*übergebene Position verändern Code
+
+        position = Spielbrett.getUserPosition()+ (int)(Math.random()*10)+1
+        if (position > 47){
+        positon = 47
+        }else{
+        Spielbrett.setUserPosition(position);
+        }
+        * */
+        System.out.println("Button funktioniert");
+                    //Übergang zurück ins Spiel
+                    setContentView(R.layout.activity_menu);
+
+                }
+
+        });
+    /*
+         Thread timer = new Thread(){
 
             public void run(){
                 try{
-                    sleep(5000); //Dauer der Gif Animation / "Splash-Screen"
+                    sleep(3000); //Dauer der Gif Animation
+                    System.out.println("Timer");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally {
@@ -44,21 +63,21 @@ public class Portal extends AppCompatActivity implements View.OnClickListener {
 
         };
         timer.start();;
+        */
+
 
     }
 
-    @Override
+    /*@Override
     protected void onPause(){
         super.onPause();
         finish();
-    }
+    }*/
 
 
-    @Override
-    public void onClick(View v) {
-        //übergebene Position verändern Code
 
-        //Übergang zurück ins Spiel
-        setContentView(R.layout.activity_menu);
-    }
+
+
 }
+
+
