a78f4d389e16cbfd00786469f90df47f3cc4bdcc
Amsi11 <jasmin.stuck@live.de>
2017-04-24 17:10:31 +0200

    die Berechnung für 0 und 1 hinzugefügt

--- app/src/main/java/t_industries/monstersandportals/EventField.java
+++ app/src/main/java/t_industries/monstersandportals/EventField.java
@@ -43,7 +43,6 @@ public class EventField extends AppCompatActivity {
 
     public void checkodd(View v){
         do {
-
             number=number%2;
             if (number == 1) {
                 //Spieler bewegen und zurück
