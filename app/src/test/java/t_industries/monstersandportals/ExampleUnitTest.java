8afc29217f7854eb27e91fc6c08e428380238398
SaW2017 <littlemiss0085@yahoo.de>
2017-06-19 11:25:22 +0200

    Änderung wegen Sonarqube

--- app/src/test/java/t_industries/monstersandportals/ExampleUnitTest.java
+++ app/src/test/java/t_industries/monstersandportals/ExampleUnitTest.java
@@ -13,5 +13,6 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+        throw new Exception("Example Unit Test schlufg fehl");
     }
 }
